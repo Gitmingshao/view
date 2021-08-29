f51436f52b68436645362df871a1bdad04586751
独为伊人醉 <1245074510@qq.com>
2021-08-29 17:00:06 +0800

    test1

--- app/src/main/java/com/wjm/myapplication/MainActivity.kt
+++ app/src/main/java/com/wjm/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.wjm.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wjm.view.DiyView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
